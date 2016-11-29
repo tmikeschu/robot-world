957829b043553565ca5e30a77bbe07be7df07f04
Mike Schutte <tmikeschutte@gmail.com>
2016-11-29 15:09:08 -0700

    develops create and read

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,8 +3,29 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    redirect '/robots'
+  end
+
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
-end
+  
+  get '/robots/new' do
+    erb :new
+  end
 
+  get '/robots/:id' do
+    @robot = Robot.find(params[:id])
+    erb :show
+  end
+
+
+  post '/robots' do 
+    robot = Robot.create(params[:robot])
+    robot.save
+    redirect '/robots'
+  end
+
+end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,8 +1,48 @@
 require 'sqlite3'
 
 class Robot
+
+  attr_reader :id,
+              :name,
+              :city,
+              :state,
+              :department
+
   def initialize(robot_params)
+    @id                       = robot_params["id"] if robot_params["id"]
+    @name                     = robot_params["name"]
+    @city                     = robot_params["city"]
+    @state                    = robot_params["state"]
+    @department               = robot_params["department"]
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
   end
+
+  def self.create(robot_params)
+    new(robot_params)
+  end
+
+  def save
+    @database.execute("INSERT INTO robots (name, city, state, department)
+                       VALUES (?,?,?,?);",
+                       @name, @city, @state, @department
+                     )
+  end
+
+  def self.all
+    robots = database.execute("SELECT * FROM robots")
+    robots.map { |robot| Robot.new(robot)}
+  end
+
+  def self.find(id)
+    robot = database.execute("SELECT * FROM robots WHERE id = ?;", id).first
+    Robot.new(robot)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/robot_world_development.db')
+    database.results_as_hash = true
+    database
+  end
+
 end
