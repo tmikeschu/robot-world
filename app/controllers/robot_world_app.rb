fe5ea1f44cf49ccfa6d155c1c28784153c20cd5b
Mike Schutte <tmikeschutte@gmail.com>
2016-11-30 06:35:01 -0700

    adds error layout page

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -4,6 +4,10 @@ class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
   set :method_override, true
 
+  not_found do
+    erb :error
+  end
+  
   get '/' do
     redirect '/robots'
   end
